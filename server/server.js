0e15c85a7fdf4d7301ab7cf0aba888637268d4f2
unknown <raaba21@gmail.com>
2024-03-27 18:23:33 -0400

    static.json

--- server/server.js
+++ server/server.js
@@ -18,13 +18,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("*", (req, res) => {
-  let url = path.join(__dirname, '../client/build', 'index.html');
-  if (!url.startsWith('/app/')) // since we're on local windows
-    url = url.substring(1);
-  res.sendFile(url);
-});
-
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
